990c9d5470bd7f13f37c3eee242276b4eae49e1d
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2026-04-28 00:06:45 -0300

    [ADD] Views Index e Contato

--- iniciando_django/iniciando_django/settings.py
+++ iniciando_django/iniciando_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z4_zd6ks7^z(cjgai&-otro!%9u&vl8=vl!g@)nef3#4&_@10l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
