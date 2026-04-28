0f09a16e0524f2d3ee1e63ebeebdc6a0b7cb2bf0
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2026-04-28 15:18:23 -0300

    [ADD] Adicionando rota para produto. Trabalhando com parâmetros na view e direcionando para o template

--- iniciando_django/core/views.py
+++ iniciando_django/core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from core.models import Produto
 def index(request):
-    return render(request, 'index.html')
+    context = {'produtos': Produto.objects.all()}
+    return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, produto_id):
+    context = {'produto': Produto.objects.get(id=produto_id)}
+    return render(request, 'produto.html', context)
