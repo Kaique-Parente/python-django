61f874d8cfeb65948d9681ece353ddde586cf335
Kaique Parente da Silva <kaique.parente.game@outlook.com>
2026-04-28 01:08:06 -0300

    [ADD] Produto e Cliente com Models/Migrations

--- iniciando_django/core/models.py
+++ iniciando_django/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço', max_digits=5, decimal_places=2)
+    estoque = models.IntegerField('Quantidade em estoque', )
+
+    def __str__(self):
+        return self.nome
+
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('Email', max_length=100)
+    telefone = models.CharField('Telefone', max_length=100)
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome}'
